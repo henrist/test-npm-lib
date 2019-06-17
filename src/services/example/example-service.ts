96ba63fbdaefa4892301dee0bb930822f72ba19d
Henrik Steen <henrist@henrist.net>
2019-06-17 08:38:15 +0200

    feat: add minus
    (Actually uses plus to make a later fix commit.)

--- src/services/example/example-service.ts
+++ src/services/example/example-service.ts
@@ -1,2 +1,3 @@
 export const multiply = (a: number, b: number): number => a * b;
 export const plus = (a: number, b: number): number => a + b;
+export const minus = (a: number, b: number): number => a + b;
